7a7a8eb1ae70c9ff072a2ab385ff150fe933808f
winsy <wiyu@ucdavis.edu>
2024-07-15 17:21:49 -0700

    imported react template

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import React from 'react';
-import Header from './components/Header';
-import About from './components/About';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
-import { CssBaseline } from '@mui/material';
-import { Container, Box } from '@mui/system';
+import LandingPage from './LandingPage';
 
 function App() {
   return (
     <div>
-      <CssBaseline />
-      <Header />
-      <Container>
-        <Box my={4}>
-          <About />
-          <Projects />
-          <Contact />
-        </Box>
-      </Container>
+      <LandingPage />
     </div>
   );
 }
